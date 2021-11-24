6908ac461a1d309fde311f43c7895a34b384cefb
brecht <brecht.porrez@ordina.be>
2021-11-24 12:56:29 +0100

    Initial commit

---
+++ src/main/java/be/ordina/introtoazure/IntroToAzureApplication.java
@@ -0,0 +1,13 @@
+package be.ordina.introtoazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroToAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroToAzureApplication.class, args);
+	}
+
+}
